5a69a5d74a53eb5107a4cebea999d859ee33020b
David <davidbrundukov228@gmail.com>
2025-07-09 15:21:14 +0300

    bag

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import os
 def main():
 	print(os.listdir('.'))
 	print('fine')
-	with open('data/text.txt', 'w') as file:
+	with open('/data/text.txt', 'w') as file:
 		file.write('Hello, World!')
-	with open('data/text.txt', 'r') as file1:
+	with open('/data/text.txt', 'r') as file1:
 		print(file.read())
 
 if __name__ == "__main__":
