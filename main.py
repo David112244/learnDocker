f49b58ee44462a1eab245c687dc35daa612a4fce
David <davidbrundukov228@gmail.com>
2025-07-09 15:25:39 +0300

    check

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import os
 
 
 def main():
-	print(os.listdir('.'))
-	print('fine')
-	with open('/data/text.txt', 'w') as file:
-		file.write('Hello, World!')
+	try:
+		print(os.listdir('/data'))
+	except:
+		print('bag')
+	# with open('/data/text.txt', 'w') as file:
+	# 	file.write('Hello, World!')
 	with open('/data/text.txt', 'r') as file1:
 		print(file1.read())
 
